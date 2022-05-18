e213e139262a0cc7e4ff196743cb33e19e46d1a4
lakdodo <daniel_itame@hotmail.com>
2022-05-18 15:50:47 -0300

    Final version

--- src/Telegram/Client.py
+++ src/Telegram/Client.py
@@ -4,21 +4,23 @@ import os
 
 
 class Client:
-
-    def __init__(self):
+    """ The constructor will initiate with these 2 attributes, the telegram token and
+     the target chat, that will receive the tweets."""
+    def __init__(self, TELE_TOKEN, CHAT_ID):
         self.token = TELE_TOKEN
         self.chat_id = CHAT_ID
-
-    def telegram_authorization(TELE_TOKEN):
+    """This authorization is needed to create the client in the main module"""
+    def telegram_authorization(self, TELE_TOKEN):
         # Autenticação do bot Telegram com telebot
         bot = telebot.TeleBot(f'{TELE_TOKEN}')
         return bot
-
-    def send_message(bosses, bot, CHAT_ID):
+    """The send_message function will send the tweets encapsulated"""
+    def send_message(self, bosses, bot, CHAT_ID):
 
         for item in bosses['data']:
             bot.send_message(chat_id=CHAT_ID, text=item['text'])
         bot.send_message(chat_id=CHAT_ID, text='-' * 20)
+
 
 if __name__ == '__main__':
     main()
--- src/Twitter/Client.py
+++ src/Twitter/Client.py
@@ -5,21 +5,31 @@ import os
 
 class Client:
 
-    def __init__(self):
+    """The constructor of this client will initiate with the bearer token,
+    to authenticate the request"""
+
+    def __init__(self, BEARER_TOKEN):
         self.token = BEARER_TOKEN
 
-    def twitter_authorization(token):
+    """This function will create the arguments to do the request procedure"""
+
+    def twitter_authorization(self, BEARER_TOKEN):
         url = "https://api.twitter.com/2/tweets/search/recent?query=from:JagexClock Amlodd"
         # parâmetros de requisição
         payload = {}
         headers = {
-            'Authorization': f'{token}',
+            'Authorization': f'{BEARER_TOKEN}',
             'Cookie': 'guest_id=v1%3A165048824367222984'}
+
         return url, payload, headers
 
-    def boss_request(url, headers, payload):
+    """This function do de request, with the parameters and arguments setted in 
+    the twitter_authorization function"""
+
+    def boss_request(self, url, headers, payload):
         boss = requests.request("GET", url, headers=headers, data=payload)
         bosses = boss.json()
+
         return bosses
 
 if __name__ == '__main__':
